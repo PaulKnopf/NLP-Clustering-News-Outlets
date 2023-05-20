f8fff9e2e40d87c46ad35c7241e3fc8df8df3e52
zoelai428 <113202967+zoelai428@users.noreply.github.com>
2023-05-20 20:58:46 +0200

    combine datasets & plot

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,25 +1,90 @@
-import pandas
+import pandas as pd
+from datetime import date
+import matplotlib.pyplot as plt
+import numpy as np
 
-
+# This function combines our 6 csv datasets from different channels into 1
 def process_data():
-    csv_file = None
-    # TODO: add additional column 'channel' to each data set
-    # TODO: combine all the data sets
-    # TODO: group by 'Title' & ' Description' with max ' Views'
-    # note the messed up column names -> ' Description' (white spaces)
-    # TODO: output as CSV-file to designated folder with current date in name
+    # our filenames are predetermined:
+    filenames = ["Al-Jazeera-English", "BBC-News", "CCTV-Video-News-Agency", "CNN-News", "DW-News", "Fox-News"]
+
+    # initialize an empty dataframe
+    df = pd.DataFrame()
+
+    # combine all the datasets
+    for name in filenames:
+        df1 = pd.read_csv("..\\data\\" + name + ".csv")
+        # clean leading and trailing whitespaces, if any
+        for col_name in df1.columns:
+            col_name.strip()
+        # add additional column 'Channel' to each data set
+        df1["Channel"] = name
+        df = pd.concat([df, df1])
+
+    # group by 'Title' & ' Description', and sort by descending ' Views'
+    df.groupby(by=['Title', ' Description'])
+    df.sort_values(by=[' Views'], ascending=False)
+
+    # output as CSV-file to designated folder with current date in name
+    today = date.today()
+    string_today = "{}-{}-{}".format(today.day, today.month, today.year)
+    df.to_csv("..\\data\\combined-" + string_today + ".csv", index=False)
+
+    # store the csv file as a read file for output
+    csv_file = pd.read_csv("..\\data\\combined-" + string_today + ".csv")
     return csv_file
 
 
-if __name__ == '__main__':
-    dw_news_data_frame = pandas.read_csv("..\\data\\DW-News.csv")
+# This function creates plots for exploratory data analysis (EDA)
+def create_plots(csv_file):
+    df = csv_file
+    print("Dataframe dimension (#rows, #columns): ")
+    print(df.shape)
 
-    print(dw_news_data_frame.columns.tolist())
-    dw_news_sorted = dw_news_data_frame.sort_values(by=['Title'])
+    # Set the range intervals for the x-axis
+    view_ranges = [0, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 3000000, np.inf]
+    labels = ['0-1K', '1K-5K', '5K-10K', '10K-50K', '50K-100K', '100K-500K', '500K-1M', '1M-3M', '3M+']
 
-    titles = dw_news_data_frame['Title'].unique()
-    processed_data = dw_news_data_frame.groupby(by=['Title', ' Description']).max()
-    # print(titles)
-    # print(len(titles))
-    # print("sorted data: ", dw_news_sorted)
-    print(processed_data)
+    # Group the data by channel and count the frequencies for each view range
+    channel_counts = df.groupby('Channel')[' Views'].apply(lambda x: np.histogram(x, bins=view_ranges)[0])
+    num_channels = len(channel_counts)
+
+    # Create the line graph for each channel
+    for i, (channel, counts) in enumerate(channel_counts.items()):
+        plt.plot(labels, counts, marker='o', label=channel)
+
+    # Set the labels and title
+    plt.xlabel('Views Range')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Views Range by Channel')
+
+    # Rotate the x-axis labels if needed
+    plt.xticks(rotation=45)
+
+    # Add legend
+    plt.legend()
+
+    # Adjust the figure size to show the legend properly
+    fig = plt.gcf()
+    fig.set_size_inches(12, 10)
+
+    # Save the plot
+    plt.savefig('..\\graphs\\views-by-channel.png')
+
+    # Display the chart
+    plt.show()
+
+create_plots(process_data())
+
+# if __name__ == '__main__':
+#     dw_news_data_frame = pd.read_csv("..\\data\\DW-News.csv")
+#
+#     print(dw_news_data_frame.columns.tolist())
+#     dw_news_sorted = dw_news_data_frame.sort_values(by=['Title'])
+#
+#     titles = dw_news_data_frame['Title'].unique()
+#     processed_data = dw_news_data_frame.groupby(by=['Title', ' Description']).max()
+#     # print(titles)
+#     # print(len(titles))
+#     # print("sorted data: ", dw_news_sorted)
+#     print(processed_data)
